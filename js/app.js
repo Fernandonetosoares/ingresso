8b985024d5d3683c66ffae851f41968228444a8d
Seu Nome <fernandonetosoares@hotmail.com>
2024-08-14 13:07:08 -0300

    decrementando os valores

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
 function comprar(){
 
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
-    alert(tipo.value);
-    alert(qtd.value);
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo.value == 'pista') {
+        comprarPista(qtd)
+    }else if (tipo.value == 'superior') {
+        comprarSuperior(qtd)
+    }else if (tipo.value == 'inferior') {
+        comprarInferior(qtd)
+    }
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível')
+    }else{
+        qtdPista = qtdPista - qtd
+        document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Compra Realizada com sucesso')
+    }
+}
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível')
+    }else{
+        qtdSuperior = qtdSuperior - qtd
+        document.getElementById('qtd-superior').textContent = qtdSuperior
+        alert('Compra Realizada com sucesso')
+    }
+}
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível')
+    }else{
+        qtdInferior = qtdInferior - qtd
+        document.getElementById('qtd-inferior').textContent = qtdInferior
+        alert('Compra Realizada com sucesso')
+    }
 }
